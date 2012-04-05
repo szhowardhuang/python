d4aa9b0cf883b66b483882dfb12112def4155c4d
Howard Huang <szhowardhuang@gmail.com>
2012-04-05 21:36:43 +0800

    add startmud.py

---
+++ startmud.py
@@ -0,0 +1,16 @@
+
+import os
+
+result = False
+f = os.popen('ps -u howard')
+data = [ eachLine.strip() for eachLine in f ]
+f.close()
+for eachLine in data:
+    if eachLine.find('driver') != -1:
+        result = True
+        print eachLine
+        break
+
+if(not result):
+    os.chdir('/home/howard/fs2')
+    os.system('driver config.fs &')
