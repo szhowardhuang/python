b57df10b1b0e4be9295fc47905cbbcabf45a5867
Howard Huang <szhowardhuang@gmail.com>
2012-04-07 18:58:50 +0800

    refine startmud.py,let it can while loop

--- if.py
+++ if.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#! /usr/bin/env python
 # Filename : if.py
 
 number=23
--- startmud.py
+++ startmud.py
@@ -1,16 +1,19 @@
-
+#! /usr/bin/env python
 import os
+import time
 
-result = False
-f = os.popen('ps -u howard')
-data = [ eachLine.strip() for eachLine in f ]
-f.close()
-for eachLine in data:
-    if eachLine.find('driver') != -1:
-        result = True
-        print eachLine
-        break
-
-if(not result):
-    os.chdir('/home/howard/fs2')
-    os.system('driver config.fs &')
+while True:
+    result = False
+    f = os.popen('ps -u howard')
+    data = [ eachLine.strip() for eachLine in f ]
+    f.close()
+    for eachLine in data:
+        if eachLine.find('driver') != -1:
+            result = True
+            print eachLine
+            break
+    
+    if(not result):
+        os.chdir('/home/howard/fs2')
+        os.system('driver config.fs &')
+    time.sleep(30)
